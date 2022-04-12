6179bfb37e304df0afdec9eddae557363f7eee20
Or Kravitz <imorkravitz@gmail.com>
2022-04-12 23:19:53 +0300

    card and home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from "@angular/router";
 
 import { LoginComponent } from "./components/auth/login/login.component";
 import { SignupComponent } from "./components/auth/signup/signup.component";
+import { HomeComponent } from "./components/home/home.component"
 
 
 const routes: Routes = [
   { path: "login", component: LoginComponent },
-  { path: "signup", component: SignupComponent }
+  { path: "signup", component: SignupComponent },
+  { path: "home", component: HomeComponent },
 ];
 
 @NgModule({
---
+++ src/app/components/nav/nav.component.ts
@@ -0,0 +1,47 @@
+import { Component , OnInit, OnDestroy} from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+import { AuthService } from '../auth/auth.service'
+import { Subscription } from 'rxjs'
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit, OnDestroy{
+
+  userIsAuthenticated = false;
+  private authListenerSubs: Subscription;
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver, private authService: AuthService) {
+    this.authListenerSubs = Subscription.EMPTY;
+
+  }
+
+  ngOnInit() {
+    this.userIsAuthenticated = this.authService.getIsAuth();
+    this.authListenerSubs = this.authService
+      .getAuthStatusListener()
+      .subscribe(isAuthenticated => {
+        this.userIsAuthenticated = true;
+        this.userIsAuthenticated = isAuthenticated;
+      });
+  }
+
+  onLogout() {
+    this.authService.logout();
+  }
+
+  ngOnDestroy() {
+    this.authListenerSubs.unsubscribe();
+  }
+
+}

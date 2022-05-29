e0a3dfa8a9faa666aabf6026d2ca230e26f51db5
Voiko <nadavv10@walla.com>
2022-05-29 21:32:52 +0300

    send the token to escrow project

--- src/app/components/contract/my-contract/contract.component.ts
+++ src/app/components/contract/my-contract/contract.component.ts
@@ -4,25 +4,33 @@ import { ActivatedRoute, ParamMap } from '@angular/router';
 import { ContractService } from '../contract.service';
 import { Router } from '@angular/router';
 import { NotifierService } from '../../notifier/notifier.service';
+import { AuthService } from '../../auth/auth.service';
 
 @Component({
   selector: 'app-contract',
   templateUrl: './contract.component.html',
   styleUrls: ['./contract.component.css']
 })
-export class ContractComponent{
+export class ContractComponent implements OnInit{
   minDate = new Date();
   contractId: string | null | undefined;
+  token: string | null="";
 
   constructor(public contractService: ContractService,
     private notificationService: NotifierService,
-    private router: Router) {
+    private router: Router,
+    private authService: AuthService) {
     }
+  ngOnInit(): void {
+    this.token = this.authService.getTokenFromSessionStorage();
+
+  }
 
   onAddContract(form : NgForm){
      if(form.invalid){
        return;
      }
+
 
     this.contractService.addContract(form.value.description,
     form.value.depositSeller, form.value.depositBuyer, form.value.walletAddressSeller,
@@ -30,3 +38,7 @@ export class ContractComponent{
     form.value.creator, form.value.buyerId);
   }
 }
+function ngOnInit() {
+  throw new Error('Function not implemented.');
+}
+
